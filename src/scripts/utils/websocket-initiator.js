0e235c8b53a02a69dc25a46655a9a3aa3d493983
samx23 <skalexsong@gmail.com>
2021-01-18 21:16:29 +0700

    fix notification caller

--- src/scripts/utils/websocket-initiator.js
+++ src/scripts/utils/websocket-initiator.js
@@ -1,3 +1,6 @@
+import NotificationHelper from './notification-helper';
+import CONFIG from '../globals/config';
+
 const WebSocketInitiator = {
   init(url) {
     const webSocket = new WebSocket(url);
@@ -5,9 +8,16 @@ const WebSocketInitiator = {
   },
 
   _onMessageHandler(message) {
-    console.log(message.data);
+    const movie = message.data;
+    NotificationHelper.sendNotification({
+      title: movie,
+      options: {
+        // kesalahan materi, perlu perubahan konfigurasi, jadi skip dulu
+        body: movie.overview,
+        image: `${CONFIG.BASE_IMAGE_URL + movie.poster_path}`,
+      },
+    });
   },
-
 };
 
 export default WebSocketInitiator;
