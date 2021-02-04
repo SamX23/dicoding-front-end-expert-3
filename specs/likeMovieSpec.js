de48614f9156359f0a6edf8fe8bf21338aad42d3
samx23 <skalexsong@gmail.com>
2021-02-04 16:52:16 +0700

    add test if like button works

--- specs/likeMovieSpec.js
+++ specs/likeMovieSpec.js
@@ -1,4 +1,5 @@
 import LikeButtonInitiator from "../src/scripts/utils/like-button-initiator";
+import FavoriteMovieIdb from "../src/scripts/data/favoritemovie-idb";
 
 describe("Liking A Movie", () => {
   it("should show the like button when the movie has not been liked before", async () => {
@@ -13,5 +14,34 @@ describe("Liking A Movie", () => {
     expect(
       document.querySelector('[aria-label="like this movie"]')
     ).toBeTruthy();
+  });
+
+  it("should not show the unlike button when the movie has not been liked before", async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector("#likeButtonContainer"),
+      movie: {
+        id: 1,
+      },
+    });
+
+    expect(
+      document.querySelector('[aria-label="unlike this movie"]')
+    ).toBeFalsy();
+  });
+
+  it("should be able to like the movie", async () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector("#likeButtonContainer"),
+      movie: {
+        id: 1,
+      },
+    });
+
+    document.querySelector("#likeButton").dispatchEvent(new Event("click"));
+    const movie = await FavoriteMovieIdb.getMovie(1);
+
+    expect(movie).toEqual({ id: 1 });
   });
 });
