6bef8aae0f11060c047e34a9b463c0f6db420d93
samx23 <skalexsong@gmail.com>
2021-02-10 15:18:30 +0700

    Modify Like component based on test

--- src/scripts/views/pages/like.js
+++ src/scripts/views/pages/like.js
@@ -1,21 +1,20 @@
 import FavoriteMovies from "../../data/favoritemovie-idb";
-import { createMovieItemTemplate } from "../templates/template-creator";
+import FavoriteMovieSearchView from "./liked-movies/favorite-movie-search-view";
+import FavoriteMovieShowPresenter from "./liked-movies/favorite-movie-show-presenter";
+import FavoriteMovieSearchPresenter from "./liked-movies/favorite-movie-search-presenter";
+
+const view = new FavoriteMovieSearchView();
 
 const Like = {
   async render() {
-    return `
-    <div class="content">
-      <h2 class="content__heading">Your Liked Movie</h2>
-      <div id="movies" class="movies"></div>
-    </div>
-    `;
+    return view.getTemplate();
   },
 
   async afterRender() {
-    const movies = await FavoriteMovies.getAllMovies();
-    const moviesContainer = document.querySelector("#movies");
-    movies.forEach((movie) => {
-      moviesContainer.innerHTML += createMovieItemTemplate(movie);
+    new FavoriteMovieShowPresenter({ view, favoriteMovies: FavoriteMovies });
+    new FavoriteMovieSearchPresenter({
+      view,
+      favoriteMovies: FavoriteMovies,
     });
   },
 };
